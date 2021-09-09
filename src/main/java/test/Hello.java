bd8a438b61c8baeee08aa38dbb7ab62e7346b026
saber <saber@fate.com>
2021-09-10 02:17:47 +0800

    fix-hot commit

--- src/main/java/test/Hello.java
+++ src/main/java/test/Hello.java
@@ -5,5 +5,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("fix-hot modified");
+        System.out.println("fix-hot modified");
+
     }
 }
